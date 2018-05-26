cc5fe763d3464b9b86592d82b38bb9e31378d8ab
jolvin <cuishuo812@gmail.com>
2018-05-26 14:11:46 +0800

    Build 20180526

---
+++ Sort/Insertion Sort/Insertion Sort.c
@@ -0,0 +1,41 @@
+//Insertion Sort
+//直接插入排序
+//Version 1.0 20180526
+//2018-05-26 13:58:35
+//CopyRight ©2018 CuiShuo. All rights reserved.
+
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    int length, t, j;
+    double duration;
+    clock_t start, finish;
+    int a[] = {49,38,65,97,76,13,27,49};
+
+    length = sizeof(a)/4;
+
+    start = clock();
+    for (int i = 1; i < length; ++i) {
+        if (a[i] < a[i-1]) {                //前面的元素比后面大
+            t = a[i];
+
+            for (j = i-1; a[j] > t; --j) {
+                a[j+1] = a[j];              //向后移
+            }                               //直到找到没有比比较项大的就退出当前循环
+            a[j+1] = t;
+        }
+    }
+    finish = clock();
+    duration = (double)(finish-start);
+
+    printf("排序结果:");
+    for (int k = 0; k < length; ++k) {
+        printf("%d ", a[k]);
+    }
+    printf("\n");
+
+    printf("直接插入排序所需时间: %lf", duration/CLOCKS_PER_SEC);
+
+    return 0;
+}
