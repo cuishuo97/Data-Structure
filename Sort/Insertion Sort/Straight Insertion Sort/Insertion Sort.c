f202f7dc7b9fc33b89040ccb1621eaf93c43b408
jolvin <cuishuo812@gmail.com>
2018-05-29 14:06:10 +0800

    Build 20180529

--- Sort/Insertion Sort/Straight Insertion Sort/Insertion Sort.c
+++ Sort/Insertion Sort/Straight Insertion Sort/Insertion Sort.c
@@ -1,7 +1,7 @@
 //Insertion Sort
 //直接插入排序
-//Version 1.0 20180526
-//2018-05-26 13:58:35
+//Version 1.1 20180529
+//2018-05-29 14:04:35
 //CopyRight ©2018 CuiShuo. All rights reserved.
 
 #include <stdio.h>
@@ -20,7 +20,7 @@ int main() {
         if (a[i] < a[i-1]) {                //前面的元素比后面大
             t = a[i];
 
-            for (j = i-1; a[j] > t; --j) {
+            for (j = i-1; a[j] > t && t >= 0; --j) {  //t>=0 防止下标越界
                 a[j+1] = a[j];              //向后移
             }                               //直到找到没有比比较项大的就退出当前循环
             a[j+1] = t;
